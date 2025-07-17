679116d970c79121b0a40872cb295698f02f3f97
charan-the-dev <mailto.charany@gmail.com>
2025-07-17 12:35:17 +0530

    configure a new dir for storing data

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ from store import init_db, store_comments_to_db
 from data_fetching import fetch_pr_comments_for_repo
 import sqlite3
 import pandas as pd
+import os
 
 if __name__ == '__main__':
     repo_name = "freeCodeCamp/freeCodeCamp"
     max_comments = 30
+    database_name = "github_pr_comments.db"
 
     # Initialize the Database
     init_db()
@@ -15,10 +17,10 @@ if __name__ == '__main__':
     print(f"Fetched {len(comments)} PR comments from {repo_name}.")
 
     # Store the comments in the database
-    store_comments_to_db(comments)
+    store_comments_to_db(comments, os.path.join("data", database_name))
 
     # Display stored comments from the Database
-    conn = sqlite3.connect("github_pr_comments.db")
+    conn = sqlite3.connect(os.path.join("data", database_name))
     df = pd.read_sql_query("SELECT * FROM pr_comments", conn)
     print(df.head())
     print(df.tail())
